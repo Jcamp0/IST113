501e471bef61445cbbbd6b558fa22b08ddf0e6a9
Jcamp0 <33106250+Jcamp0@users.noreply.github.com>
2018-03-20 18:48:18 -0400

    Task Manager

---
+++ appTask1/appTask1.js
@@ -0,0 +1,33 @@
+function runTaskManager(){
+    let taskInput = document.getElementById("taskInput");
+    let addTask = document.getElementById("addTask");
+    let taskList = document.getElementById("taskList");
+
+        addTask.addEventListener("click", function(){
+    let newElem = document.createElement("LI");
+    let newContent = document.createTextNode(taskInput.value);
+    let deleteButton = document.createElement("button");       
+    deleteButton.innerHTML = "Remove";
+    
+
+    newElem.appendChild(newContent);
+    newElem.appendChild(deleteButton);
+   
+    deleteButton.addEventListener("click", function() {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    });
+
+    
+    newElem.classList.add(taskList.value);
+  
+    
+    taskList.appendChild(newElem);
+  });
+
+}
+
+
+
+    
+
+runTaskManager();
