138b84ae25bf7a2137c13127c11b4fb47e82b912
Jcamp0 <33106250+Jcamp0@users.noreply.github.com>
2018-03-21 14:48:56 -0400

    jqLight on/off fix

--- jqLight/jqLight.js
+++ jqLight/jqLight.js
@@ -6,11 +6,13 @@ $("#switch").on("click", function(){
         
         $('#switch').css('float', 'left');
        $('body').css('background-color', 'white');
+       $('#switch').text("on");
         lightOn = false;
     }
     else {
         $('#switch').css('float', 'right');
         $('body').css('background-color', 'black');
+        $('#switch').text("off");
          lightOn = true;
     }
 });
